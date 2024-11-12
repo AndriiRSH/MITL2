4cd1a02bd6e6de2fc451ece6fbf7d78b547c0e39
Andrii Roshylo <102091946+AndriiRSH@users.noreply.github.com>
2024-11-12 10:09:25 +0200

    Add files via upload

---
+++ LabSolution.DAL/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using LabSolution.DAL.Models;
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; } 
+    public ICollection<StudentCourse> StudentCourses { get; set; }
+}
